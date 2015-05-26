975bd7f9096b33096d0b972f68286929ca01bf6d
Vincent Daubry <vdaubry@gmail.com>
2015-05-26 18:25:41 +0200

    Listen for message in PageDownloader

--- CrawlManager/models/url_frontier.rb
+++ CrawlManager/models/url_frontier.rb
@@ -1,10 +1,7 @@
 class UrlFrontier
   def add(url:)
-    url_filter = UrlFilter.new(url: url)
-      
-    if url_filter.valid_url?
-      domain = ScrapperDomain.new(url: url)
-      domain.add_url(url: url)
+    if UrlFilter.new(url: url).valid_url?
+      ScrapperDomain.new(url: url).add_url(url: url)
     end
     
     ScrapperBloomFilter.new.insert(key: url)
--- PageDownloader/workspace/src/github.com/vdaubry/page_downloader.go
+++
@@ -1,6 +0,0 @@
-package main
-
-func main() {
-  rabbitClient := NewRabbitClient("amqp://guest:guest@54.83.30.48:5672/")
-  rabbitClient.Listen("download_page")
-}
