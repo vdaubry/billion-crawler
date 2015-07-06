6d5fc04c73dd8038e6b944bb4e0468b3dbc17931
Vincent Daubry <vdaubry@gmail.com>
2015-07-06 21:52:04 +0200

    add a safe URI join

--- CrawlManager/lib/safe_uri.rb
+++ CrawlManager/lib/safe_uri.rb
@@ -2,7 +2,7 @@ class SafeUri
   def self.join(base_url:, path:)
     begin
       URI.join(base_url, URI.encode(path)).to_s if path
-    rescue URI::InvalidURIError, NoMethodError => e
+    rescue StandardError => e
       $LOG.error e
       nil
     end
